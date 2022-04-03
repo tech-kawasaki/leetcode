c07baf47b4ed28e51fbe5f0b3a8be257fd2f435d
Kawasaki Hirotsugu <36406175+tech-kawasaki@users.noreply.github.com>
2022-04-03 20:11:40 +0900

    add_two_number

---
+++ add_two_number.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        return self.recursive(l1, l2, 0,ListNode(0))
+
+    def recursive(self, l1: Optional[ListNode], l2: Optional[ListNode], carry: int, tail: ListNode) -> tuple(ListNode, int):
+        if not(l1 or l2):
+            return None
+        val1 = (l1.val if l1 else 0)
+        val2 = (l2.val if l2 else 0)
+
+        carry, out = divmod(val1 + val2 + carry, 10)
+        tail.next = ListNode(out)
+
+        self.recursive((l1.next if l1 else None), (l2.next if l2 else None), carry, tail.next)
+        return tail
---
+++ add_two_number_recursive.py
@@ -0,0 +1,35 @@
+# 自己流で汚い
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        tail = ListNode(0)
+        result = self.recursive(l1, l2, 0, tail)
+        if (result == tail):
+            result = result.next
+        return result
+
+    def recursive(self, l1: Optional[ListNode], l2: Optional[ListNode], carry: int, tail: ListNode) -> ListNode:
+        if not(l1 or l2):
+            if carry > 0:
+                tail.next = ListNode(carry)
+                return tail
+            else:
+                return None
+            
+        val1 = (l1.val if l1 else 0)
+        val2 = (l2.val if l2 else 0)
+
+        carry, out = divmod(val1 + val2 + carry, 10)
+        tail.next = ListNode(out)
+
+        self.recursive((l1.next if l1 else None), (l2.next if l2 else None), carry, tail.next)
+        return tail
