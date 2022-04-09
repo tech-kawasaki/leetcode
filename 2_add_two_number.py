4db0e6d56020a6299a43bab97b47a5d799b95360
Kawasaki Hirotsugu <36406175+tech-kawasaki@users.noreply.github.com>
2022-04-10 08:59:53 +0900

    iteration

--- 2_add_two_number.py
+++ 2_add_two_number.py
@@ -7,22 +7,25 @@ class ListNode(object):
         self.next = None
 
 class Solution(object):
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]):
+    def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
-        return self.recursive(l1, l2, 0,ListNode(0))
-
-    def recursive(self, l1: Optional[ListNode], l2: Optional[ListNode], carry: int, tail: ListNode) -> tuple(ListNode, int):
-        if not(l1 or l2):
-            return None
-        val1 = (l1.val if l1 else 0)
-        val2 = (l2.val if l2 else 0)
-
-        carry, out = divmod(val1 + val2 + carry, 10)
-        tail.next = ListNode(out)
-
-        self.recursive((l1.next if l1 else None), (l2.next if l2 else None), carry, tail.next)
-        return tail
+        result = ListNode(0)
+        result_tail = result
+        carry = 0
+                
+        while l1 or l2 or carry:            
+            val1  = (l1.val if l1 else 0)
+            val2  = (l2.val if l2 else 0)
+            carry, out = divmod(val1+val2 + carry, 10)    
+                      
+            result_tail.next = ListNode(out)
+            result_tail = result_tail.next                      
+            
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+               
+        return result.next
