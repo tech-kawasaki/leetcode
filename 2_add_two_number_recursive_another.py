8ede5d7d65b1bbd730737ecbe2959c8004d067a6
Kawasaki Hirotsugu <36406175+tech-kawasaki@users.noreply.github.com>
2022-04-09 19:35:09 +0900

    fix 2

--- 2_add_two_number_recursive_another.py
+++ 2_add_two_number_recursive_another.py
@@ -15,9 +15,7 @@ class Solution:
         ret = ListNode(val % 10 ) 
         
         if l1.next or l2.next or c:
-            if not l1.next:
-                l1.next = ListNode(0)
-            if not l2.next:
-                l2.next = ListNode(0)
+            l1.next = l1.next if l1.next else ListNode(0)
+            l2.next = l2.next if l2.next else ListNode(0)
             ret.next = self.addTwoNumbers(l1.next,l2.next,c)
         return ret
