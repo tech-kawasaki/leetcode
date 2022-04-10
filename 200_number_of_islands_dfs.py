492a4622d6c0045f130910e96681fa90d41bdb49
Kawasaki Hirotsugu <36406175+tech-kawasaki@users.noreply.github.com>
2022-04-11 07:35:04 +0900

    fix 200

--- 200_number_of_islands_dfs.py
+++ 200_number_of_islands_dfs.py
@@ -1,9 +1,6 @@
 class Solution:
     def dfs(self, grid: list[list[str]], r: int, c: int):
-        nr = len(grid)
-        nc = len(grid[0])
-        
-        if (r < 0) or (c < 0) or (r >= nr) or (c >= nc) or (grid[r][c] == "0"):
+        if (r < 0) or (c < 0) or (r >= len(grid)) or (c >= len(grid[0])) or (grid[r][c] == "0"):
             return
         
         grid[r][c] = "0"
@@ -13,14 +10,12 @@ class Solution:
         self.dfs(grid, r, c+1)
     
     def numIslands(self, grid: list[list[str]]) -> int:
-        if (grid == None) or (len(grid) == 0):
+        if not grid:
             return 0
-        
-        nr = len(grid)
-        nc = len(grid[0])
+
         num_islands = 0
-        for r in range(nr):
-            for c in range(nc):
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
                 if grid[r][c] == "1":
                     num_islands += 1
                     self.dfs(grid, r, c)
