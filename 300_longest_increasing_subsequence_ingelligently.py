197f48b409b135095b289c3877f1c18a75fe9851
Kawasaki Hirotsugu <36406175+tech-kawasaki@users.noreply.github.com>
2022-04-24 22:18:36 +0900

    add 300

---
+++ 300_longest_increasing_subsequence_ingelligently.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLIS(self, nums: list[int]) -> int:
+        sub = [nums[0]]
+        
+        for num in nums[1:]:
+            if num > sub[-1]:
+                sub.append(num)
+            else:
+                # Find the first element in sub that is greater than or equal to num
+                i = 0
+                while num > sub[i]:
+                    i += 1
+                sub[i] = num
+
+        return len(sub)
